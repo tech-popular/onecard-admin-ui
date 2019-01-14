ce18f6e3eb432da026d44278e8cc61932a9afd06
刘慧勇 <liuhuiyong@9fbank.cc>
2019-01-14 09:41:29 +0800

    增加上下文环境canary-admin

--- src/mock/modules/job-schedule.js
+++ src/mock/modules/job-schedule.js
@@ -1,7 +1,7 @@
 import Mock from 'mockjs'
 
 // 生成数据列表
-var dataList = []
+const dataList = []
 for (let i = 0; i < Math.floor(Math.random() * 10 + 1); i++) {
   let name = Mock.Random.name()
   dataList.push(Mock.mock({
