f2bbb80894040269c5d137be67f3598cadd3af0b
刘慧勇 <liuhuiyong@9fbank.cc>
2019-09-05 16:04:26 +0800

    租户信息页面完成

--- src/views/modules/sys/systenantuser-add-or-update.vue
+++ src/views/modules/sys/systenantuser-add-or-update.vue
@@ -1,24 +1,15 @@
 <template>
   <el-dialog
-    :title="!dataForm.id ? '新增' : '修改'"
+    title="配置信息"
     :close-on-click-modal="false"
     :visible.sync="visible">
     <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="80px">
-    <el-form-item label="租户id" prop="tenantId">
-      <el-input v-model="dataForm.tenantId" placeholder="租户id"></el-input>
-    </el-form-item>
     <el-form-item>
-      <el-transfer v-model="userGroupStr" :titles="['全部', '选中']" :props="{
+      <el-transfer v-model="userStr" :titles="['全部', '选中']" :props="{
                         key: 'userId',
                         label: 'username'
                       }" :data="allUseOptions" :filterable="true">
       </el-transfer>
-    </el-form-item>
-    <!--<el-form-item label="系统用户Id" prop="userId">
-      <el-input v-model="dataForm.userId" placeholder="系统用户Id"></el-input>
-    </el-form-item>-->
-    <el-form-item label="是否启用" prop="enable">
-      <el-input v-model="dataForm.enable" placeholder="是否启用"></el-input>
     </el-form-item>
     </el-form>
     <span slot="footer" class="dialog-footer">
@@ -34,24 +25,14 @@
       return {
         visible: false,
         dataForm: {
-          id: 0,
           tenantId: '',
-          userId: '',
-          enable: '',
-          createdTime: '',
-          updatedTime: ''
+          userIds: []
         },
         allUseOptions: [],
         userStr: [],
         dataRule: {
           tenantId: [
             { required: true, message: '租户id不能为空', trigger: 'blur' }
-          ],
-          userId: [
-            { required: true, message: '系统用户Id不能为空', trigger: 'blur' }
-          ],
-          enable: [
-            { required: true, message: '是否启用不能为空', trigger: 'blur' }
           ]
         }
       }
@@ -70,11 +51,8 @@
             }).then(({data}) => {
               if (data && data.code === 0) {
                 this.allUseOptions = data.allsysUser
-                this.dataForm.tenantId = data.sysreltenantuser.tenantId
-                this.dataForm.userId = data.sysreltenantuser.userId
-                this.dataForm.enable = data.sysreltenantuser.enable
-                this.dataForm.createdTime = data.sysreltenantuser.createdTime
-                this.dataForm.updatedTime = data.sysreltenantuser.updatedTime
+                this.dataForm.tenantId = data.info.tenantId
+                this.userStr = data.info.userIds
               }
             })
           }
@@ -82,18 +60,15 @@
       },
       // 表单提交
       dataFormSubmit () {
+        console.log(this.dataForm.userIds)
         this.$refs['dataForm'].validate((valid) => {
           if (valid) {
             this.$http({
-              url: this.$http.adornUrl(`/sys/sysreltenantuser/${!this.dataForm.id ? 'save' : 'update'}`),
+              url: this.$http.adornUrl(`/sys/systenant/tenantuser/save`),
               method: 'post',
               data: this.$http.adornData({
-                'id': this.dataForm.id || undefined,
                 'tenantId': this.dataForm.tenantId,
-                'userId': this.dataForm.userId,
-                'enable': this.dataForm.enable,
-                'createdTime': this.dataForm.createdTime,
-                'updatedTime': this.dataForm.updatedTime
+                'userIds': this.userStr
               })
             }).then(({data}) => {
               if (data && data.code === 0) {
