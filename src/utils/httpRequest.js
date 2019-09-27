e551ab3cc30d0d3abfc762903743561dcb4bd3a9
刘慧勇 <liuhuiyong@9fbank.cc>
2019-09-27 10:12:51 +0800

    chart ->modify

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -45,11 +45,6 @@ http.adornUrl = (actionName) => {
   return (process.env.NODE_ENV !== 'production' && process.env.OPEN_PROXY ? '/proxyApi/' : window.SITE_CONFIG.baseUrl) + actionName
 }
 
-
-http.adornUrl1 = (actionName) => {
-  // 非生产环境 && 开启代理, 接口前缀统一使用[/proxyApi/]前缀做代理拦截!
-  return (process.env.NODE_ENV !== 'production' && process.env.OPEN_PROXY ? '/proxyApi/' : window.SITE_CONFIG.baseUrl) + actionName
-}
 /**
  * get请求参数处理
  * @param {*} params 参数对象
