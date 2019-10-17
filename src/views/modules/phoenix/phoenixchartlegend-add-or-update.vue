09dbaf794462e47be0fab8b45f91c5dbd9e0924b
lirong <lirong@ithro.com.cn>
2019-10-17 18:36:23 +0800

    添加生成图表的sql和图表图例表

---
+++ src/views/modules/phoenix/phoenixchartlegend-add-or-update.vue
@@ -0,0 +1,102 @@
+<template>
+  <el-dialog
+    :title="!dataForm.id ? '新增' : '修改'"
+    :close-on-click-modal="false"
+    :visible.sync="visible">
+    <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="80px">
+    <el-form-item label="chart_id号" prop="chartId">
+      <el-input v-model="dataForm.chartId" placeholder="chart_id号"></el-input>
+    </el-form-item>
+    <el-form-item label="名称" prop="name">
+      <el-input v-model="dataForm.name" placeholder="名称"></el-input>
+    </el-form-item>
+    <el-form-item label="类型" prop="type">
+      <el-input v-model="dataForm.type" placeholder="类型"></el-input>
+    </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        visible: false,
+        dataForm: {
+          id: 0,
+          chartId: '',
+          name: '',
+          type: ''
+        },
+        dataRule: {
+          chartId: [
+            { required: true, message: 'chart_id号不能为空', trigger: 'blur' }
+          ],
+          name: [
+            { required: true, message: '名称不能为空', trigger: 'blur' }
+          ],
+          type: [
+            { required: true, message: '类型不能为空', trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    methods: {
+      init (id) {
+        this.dataForm.id = id || 0
+        this.visible = true
+        this.$nextTick(() => {
+          this.$refs['dataForm'].resetFields()
+          if (this.dataForm.id) {
+            this.$http({
+              url: this.$http.adornUrl(`/phoenix/phoenixchartlegend/info/${this.dataForm.id}`),
+              method: 'get',
+              params: this.$http.adornParams()
+            }).then(({data}) => {
+              if (data && data.code === 0) {
+                this.dataForm.chartId = data.phoenixchartlegend.chartId
+                this.dataForm.name = data.phoenixchartlegend.name
+                this.dataForm.type = data.phoenixchartlegend.type
+              }
+            })
+          }
+        })
+      },
+      // 表单提交
+      dataFormSubmit () {
+        this.$refs['dataForm'].validate((valid) => {
+          if (valid) {
+            this.$http({
+              url: this.$http.adornUrl(`/phoenix/phoenixchartlegend/${!this.dataForm.id ? 'save' : 'update'}`),
+              method: 'post',
+              data: this.$http.adornData({
+                'id': this.dataForm.id || undefined,
+                'chartId': this.dataForm.chartId,
+                'name': this.dataForm.name,
+                'type': this.dataForm.type
+              })
+            }).then(({data}) => {
+              if (data && data.code === 0) {
+                this.$message({
+                  message: '操作成功',
+                  type: 'success',
+                  duration: 1500,
+                  onClose: () => {
+                    this.visible = false
+                    this.$emit('refreshDataList')
+                  }
+                })
+              } else {
+                this.$message.error(data.msg)
+              }
+            })
+          }
+        })
+      }
+    }
+  }
+</script>
---
+++ src/views/modules/phoenix/phoenixchartsql-add-or-update.vue
@@ -0,0 +1,102 @@
+<template>
+  <el-dialog
+    :title="!dataForm.id ? '新增' : '修改'"
+    :close-on-click-modal="false"
+    :visible.sync="visible">
+    <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="80px">
+    <el-form-item label="chart_id号" prop="chartId">
+      <el-input v-model="dataForm.chartId" placeholder="chart_id号"></el-input>
+    </el-form-item>
+    <el-form-item label="sql语句" prop="sql">
+      <el-input v-model="dataForm.sql" placeholder="sql语句"></el-input>
+    </el-form-item>
+    <el-form-item label="标识sql对应的chart组件类型 legend、series" prop="type">
+      <el-input v-model="dataForm.type" placeholder="标识sql对应的chart组件类型 legend、series"></el-input>
+    </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        visible: false,
+        dataForm: {
+          id: 0,
+          chartId: '',
+          sql: '',
+          type: ''
+        },
+        dataRule: {
+          chartId: [
+            { required: true, message: 'chart_id号不能为空', trigger: 'blur' }
+          ],
+          sql: [
+            { required: true, message: 'sql语句不能为空', trigger: 'blur' }
+          ],
+          type: [
+            { required: true, message: '标识sql对应的chart组件类型 legend、series不能为空', trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    methods: {
+      init (id) {
+        this.dataForm.id = id || 0
+        this.visible = true
+        this.$nextTick(() => {
+          this.$refs['dataForm'].resetFields()
+          if (this.dataForm.id) {
+            this.$http({
+              url: this.$http.adornUrl(`/phoenix/phoenixchartsql/info/${this.dataForm.id}`),
+              method: 'get',
+              params: this.$http.adornParams()
+            }).then(({data}) => {
+              if (data && data.code === 0) {
+                this.dataForm.chartId = data.phoenixchartsql.chartId
+                this.dataForm.sql = data.phoenixchartsql.sql
+                this.dataForm.type = data.phoenixchartsql.type
+              }
+            })
+          }
+        })
+      },
+      // 表单提交
+      dataFormSubmit () {
+        this.$refs['dataForm'].validate((valid) => {
+          if (valid) {
+            this.$http({
+              url: this.$http.adornUrl(`/phoenix/phoenixchartsql/${!this.dataForm.id ? 'save' : 'update'}`),
+              method: 'post',
+              data: this.$http.adornData({
+                'id': this.dataForm.id || undefined,
+                'chartId': this.dataForm.chartId,
+                'sql': this.dataForm.sql,
+                'type': this.dataForm.type
+              })
+            }).then(({data}) => {
+              if (data && data.code === 0) {
+                this.$message({
+                  message: '操作成功',
+                  type: 'success',
+                  duration: 1500,
+                  onClose: () => {
+                    this.visible = false
+                    this.$emit('refreshDataList')
+                  }
+                })
+              } else {
+                this.$message.error(data.msg)
+              }
+            })
+          }
+        })
+      }
+    }
+  }
+</script>
