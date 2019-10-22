6ae957e329040b486a691a8f35f18f1dab451218
wyz <wyz136819518@163.com>
2019-10-22 17:29:43 +0800

    添加选择项页面以及更新刻度表页面

--- src/views/modules/phoenix/phoenixselection.vue
+++ src/views/modules/phoenix/phoenixselection.vue
@@ -1,4 +1,7 @@
 <template>
+  <el-dialog
+    title="大屏选择项"
+    :visible.sync="visible">
   <div class="mod-config">
     <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
       <el-form-item>
@@ -6,7 +9,7 @@
       </el-form-item>
       <el-form-item>
         <el-button @click="getDataList()">查询</el-button>
-        <el-button v-if="isAuth('phoenix:phoenixselection:save')" type="primary" @click="addOrUpdateHandle()">新增</el-button>
+        <el-button v-if="isAuth('phoenix:phoenixselection:save')" type="primary" @click="addOrUpdateHandle(dataForm.chartId, 0)">新增</el-button>
         <el-button v-if="isAuth('phoenix:phoenixselection:delete')" type="danger" @click="deleteHandle()" :disabled="dataListSelections.length <= 0">批量删除</el-button>
       </el-form-item>
     </el-form>
@@ -26,7 +29,7 @@
         prop="id"
         header-align="center"
         align="center"
-        label="主键">
+        label="编号">
       </el-table-column>
       <el-table-column
         prop="chartId"
@@ -65,7 +68,7 @@
         width="150"
         label="操作">
         <template slot-scope="scope">
-          <el-button type="text" size="small" @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
+          <el-button type="text" size="small" @click="addOrUpdateHandle(dataForm.chartId, scope.row.id)">修改</el-button>
           <el-button type="text" size="small" @click="deleteHandle(scope.row.id)">删除</el-button>
         </template>
       </el-table-column>
@@ -80,8 +83,9 @@
       layout="total, sizes, prev, pager, next, jumper">
     </el-pagination>
     <!-- 弹窗, 新增 / 修改 -->
-    <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></add-or-update>
+    <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList(this.dataForm.chartId)"></add-or-update>
   </div>
+  </el-dialog>
 </template>
 
 <script>
@@ -89,6 +93,7 @@
   export default {
     data () {
       return {
+        visible: false,
         dataForm: {
           key: ''
         },
@@ -105,11 +110,16 @@
       AddOrUpdate
     },
     activated () {
-      this.getDataList()
+      this.getDataList(this.dataForm.chartId)
     },
     methods: {
+      refreshData () {
+        this.getDataList(this.dataForm.chartId)
+      },
       // 获取数据列表
-      getDataList () {
+      getDataList (chartId) {
+        this.dataForm.chartId = chartId
+        this.visible = true
         this.dataListLoading = true
         this.$http({
           url: this.$http.adornUrl('/phoenix/phoenixselection/list'),
@@ -117,7 +127,8 @@
           params: this.$http.adornParams({
             'page': this.pageIndex,
             'limit': this.pageSize,
-            'key': this.dataForm.key
+            'key': this.dataForm.key,
+            'chartId': this.dataForm.chartId
           })
         }).then(({data}) => {
           if (data && data.code === 0) {
@@ -146,10 +157,10 @@
         this.dataListSelections = val
       },
       // 新增 / 修改
-      addOrUpdateHandle (id) {
+      addOrUpdateHandle (chartId, id) {
         this.addOrUpdateVisible = true
         this.$nextTick(() => {
-          this.$refs.addOrUpdate.init(id)
+          this.$refs.addOrUpdate.init(chartId, id)
         })
       },
       // 删除
@@ -173,7 +184,7 @@
                 type: 'success',
                 duration: 1500,
                 onClose: () => {
-                  this.getDataList()
+                  this.getDataList(this.dataList.chartId)
                 }
               })
             } else {
