b88dc97736e794790c4eba0061563d1bea812cc1
lirong <lirong@ithro.com.cn>
2019-10-26 18:26:27 +0800

    更新格式

--- src/views/modules/phoenix/phoenixselectiondata.vue
+++ src/views/modules/phoenix/phoenixselectiondata.vue
@@ -91,115 +91,115 @@
     import PhoenixSelectionCharts from './phoenixselection-charts-add-or-update'
 
     export default {
-        data() {
-            return {
-                visible: false,
-                dataForm: {
-                    key: ''
-                },
-                dataList: [],
-                pageIndex: 1,
-                pageSize: 10,
-                totalPage: 0,
-                dataListLoading: false,
-                dataListSelections: [],
-                addOrUpdateVisible: false,
-                phoenixSelectionChartsVisible: false
-            }
-        },
-        components: {
-            AddOrUpdate,
-            PhoenixSelectionCharts
-        },
-        activated() {
-            this.getDataList(this.dataForm.selectionId)
-        },
-        methods: {
-            // 获取数据列表
-            getDataList(selectionId) {
-                this.dataListLoading = true
-                this.visible = true
-                this.dataForm.selectionId = selectionId
-                this.$http({
-                    url: this.$http.adornUrl('/phoenix/phoenixselectiondata/list'),
-                    method: 'get',
-                    params: this.$http.adornParams({
-                        'page': this.pageIndex,
-                        'limit': this.pageSize,
-                        'key': this.dataForm.key,
-                        'selectionId': this.dataForm.selectionId
-                    })
-                }).then(({data}) => {
-                    if (data && data.code === 0) {
-                        this.dataList = data.page.list
-                        this.totalPage = data.page.totalCount
-                    } else {
-                        this.dataList = []
-                        this.totalPage = 0
-                    }
-                    this.dataListLoading = false
-                })
-            },
-            // 每页数
-            sizeChangeHandle(val) {
-                this.pageSize = val
-                this.pageIndex = 1
-                this.getDataList(this.dataForm.selectionId)
-            },
-            // 当前页
-            currentChangeHandle(val) {
-                this.pageIndex = val
-                this.getDataList(this.dataForm.selectionId)
-            },
-            // 多选
-            selectionChangeHandle(val) {
-                this.dataListSelections = val
-            },
-            // 新增 / 修改
-            addOrUpdateHandle(selectionId, id) {
-                this.addOrUpdateVisible = true
-                this.$nextTick(() => {
-                    this.$refs.addOrUpdate.init(selectionId, id)
-                })
-            },
-            //  大屏上的charts
-            phoenixioSelectionChartsHandle(id) {
-                this.phoenixSelectionChartsVisible = true
-                this.$nextTick(() => {
-                    this.$refs.phoenixSelectionCharts.init(id)
-                })
-            },
-            // 删除
-            deleteHandle(id) {
-                var ids = id ? [id] : this.dataListSelections.map(item => {
-                    return item.id
-                })
-                this.$confirm(`确定对[id=${ids.join(',')}]进行[${id ? '删除' : '批量删除'}]操作?`, '提示', {
-                    confirmButtonText: '确定',
-                    cancelButtonText: '取消',
-                    type: 'warning'
-                }).then(() => {
-                    this.$http({
-                        url: this.$http.adornUrl('/phoenix/phoenixselectiondata/delete'),
-                        method: 'post',
-                        data: this.$http.adornData(ids, false)
-                    }).then(({data}) => {
-                        if (data && data.code === 0) {
-                            this.$message({
-                                message: '操作成功',
-                                type: 'success',
-                                duration: 1500,
-                                onClose: () => {
-                                    this.visible = false
-                                    this.getDataList(this.dataForm.selectionId)
-                                }
-                            })
-                        } else {
-                            this.$message.error(data.msg)
-                        }
-                    })
-                })
-            }
+      data () {
+        return {
+          visible: false,
+          dataForm: {
+            key: ''
+          },
+          dataList: [],
+          pageIndex: 1,
+          pageSize: 10,
+          totalPage: 0,
+          dataListLoading: false,
+          dataListSelections: [],
+          addOrUpdateVisible: false,
+          phoenixSelectionChartsVisible: false
         }
+      },
+      components: {
+        AddOrUpdate,
+        PhoenixSelectionCharts
+      },
+      activated () {
+        this.getDataList(this.dataForm.selectionId)
+      },
+      methods: {
+            // 获取数据列表
+        getDataList (selectionId) {
+          this.dataListLoading = true
+          this.visible = true
+          this.dataForm.selectionId = selectionId
+          this.$http({
+            url: this.$http.adornUrl('/phoenix/phoenixselectiondata/list'),
+            method: 'get',
+            params: this.$http.adornParams({
+              'page': this.pageIndex,
+              'limit': this.pageSize,
+              'key': this.dataForm.key,
+              'selectionId': this.dataForm.selectionId
+            })
+          }).then(({data}) => {
+            if (data && data.code === 0) {
+              this.dataList = data.page.list
+              this.totalPage = data.page.totalCount
+            } else {
+              this.dataList = []
+              this.totalPage = 0
+            }
+            this.dataListLoading = false
+          })
+        },
+            // 每页数
+        sizeChangeHandle (val) {
+          this.pageSize = val
+          this.pageIndex = 1
+          this.getDataList(this.dataForm.selectionId)
+        },
+            // 当前页
+        currentChangeHandle (val) {
+          this.pageIndex = val
+          this.getDataList(this.dataForm.selectionId)
+        },
+            // 多选
+        selectionChangeHandle (val) {
+          this.dataListSelections = val
+        },
+            // 新增 / 修改
+        addOrUpdateHandle (selectionId, id) {
+          this.addOrUpdateVisible = true
+          this.$nextTick(() => {
+            this.$refs.addOrUpdate.init(selectionId, id)
+          })
+        },
+            //  大屏上的charts
+        phoenixioSelectionChartsHandle (id) {
+          this.phoenixSelectionChartsVisible = true
+          this.$nextTick(() => {
+            this.$refs.phoenixSelectionCharts.init(id)
+          })
+        },
+            // 删除
+        deleteHandle (id) {
+          var ids = id ? [id] : this.dataListSelections.map(item => {
+            return item.id
+          })
+          this.$confirm(`确定对[id=${ids.join(',')}]进行[${id ? '删除' : '批量删除'}]操作?`, '提示', {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'warning'
+          }).then(() => {
+            this.$http({
+              url: this.$http.adornUrl('/phoenix/phoenixselectiondata/delete'),
+              method: 'post',
+              data: this.$http.adornData(ids, false)
+            }).then(({data}) => {
+              if (data && data.code === 0) {
+                this.$message({
+                  message: '操作成功',
+                  type: 'success',
+                  duration: 1500,
+                  onClose: () => {
+                    this.visible = false
+                    this.getDataList(this.dataForm.selectionId)
+                  }
+                })
+              } else {
+                this.$message.error(data.msg)
+              }
+            })
+          })
+        }
+      }
     }
 </script>
