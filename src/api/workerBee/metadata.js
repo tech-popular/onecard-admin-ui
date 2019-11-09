8943a5afa1d4e3d5d14b1abe5fa065abca3e8271
wangjun1 <wangjun1@ithro.com.cn>
2019-11-09 18:34:29 +0800

    元数据管理模块模拟数据

---
+++ src/api/workerBee/metadata.js
@@ -0,0 +1,13 @@
+import { httpGet, httpPost } from '@/api'
+
+// 元数据管理
+export const metadataList = (params) => httpGet('/canary/canaryproject/list', params) // 初始化列表
+export const allRuleTemplate = () => httpGet('/cash/riskrules/allRuleTemplate') // 任务类型
+
+export const deleteArea = (params) => httpPost('/cash/area/delete', params) // 删除
+export const findAllCapitalList = () => httpGet('/cash/instmanage/findAllCapitalList') // findAllCapitalList
+export const areaListArea = () => httpGet('/cash/area/areaList')// areaListArea
+export const saveArea = (params) => httpPost('/cash/area/save', params) // 新增 > 表单提交
+export const infoArea = (params) => httpGet('/cash/area/info', params) // 修改回显
+export const deleteByCapitalId = (params) => httpPost('/cash/area/deleteByCapitalId', params) // deleteByCapitalId
+export const updateArea = (params) => httpPost('/cash/area/save', params)// 修改 > 表单提交
--- src/views/modules/workerBee/metadata.vue
+++ src/views/modules/workerBee/metadata.vue
@@ -1,6 +1,64 @@
 <template>
   <div>
-    <p>工蜂 - 元数据管理模块</p>
+    <el-form :inline="true" :model="dataForm" >
+      <el-form-item label="任务类型" prop="ruleName">
+        <el-select filterable v-model="dataForm.templatename" clearable>
+          <el-option v-for="item in templateList" :value="item.ruleName" :key="item.ruleName" :label="item.ruleNameCn"/>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="getDataList()">查询</el-button>
+        <el-button type="primary" @click="addOrUpdateHandle()">新增</el-button>
+        <el-button type="danger" @click="deleteHandle()" :disabled="dataListSelections.length <= 0">批量删除</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table
+      :data="dataList" border
+      v-loading="dataListLoading"
+      @selection-change="selectionChangeHandle"
+      style="width: 100%;">
+      <el-table-column
+        type="selection"
+        header-align="center"
+        align="center"
+        width="50"/>
+      <el-table-column
+        prop="id"
+        header-align="center"
+        align="center"
+        label="任务ID"/>
+      <el-table-column
+        prop="project"
+        header-align="center"
+        align="center"
+        label="任务类型"/>
+      <el-table-column
+        prop="serviceName"
+        header-align="center"
+        align="center"
+        label="任务名称"/>
+      <el-table-column
+        prop="group"
+        header-align="center"
+        align="center"
+        label="任务"/>
+      <el-table-column
+        prop="enable"
+        header-align="center"
+        align="center"
+        label="是否启用">
+        <template slot-scope="scope">
+          <el-tag v-if="scope.row.enable === 1" size="small" >正常</el-tag>
+          <el-tag v-else size="small" type="danger">禁用</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column fixed="right" header-align="center" align="center" width="150" label="操作">
+        <template slot-scope="scope">
+          <el-button type="text" size="small" @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
+          <el-button type="text" size="small" @click="deleteHandle(scope.row.id)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
     <el-pagination
       @size-change="sizeChangeHandle"
       @current-change="currentChangeHandle"
@@ -8,8 +66,7 @@
       :page-sizes="[10, 20, 50, 100]"
       :page-size="pageSize"
       :total="totalPage"
-      layout="total, sizes, prev, pager, next, jumper">
-    </el-pagination>
+      layout="total, sizes, prev, pager, next, jumper"/>
     <!-- 弹窗, 新增 / 修改 -->
     <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"/>
   </div>
@@ -17,13 +74,15 @@
 
 <script>
   import AddOrUpdate from './metadata-add-or-update'
+  import { metadataList, allRuleTemplate } from '@/api/workerBee/metadata'
   export default {
     data () {
       return {
         dataForm: {
-          key: ''
+          templatename: ''
         },
         dataList: [],
+        templateList: [],
         pageIndex: 1,
         pageSize: 10,
         totalPage: 0,
@@ -41,7 +100,28 @@
     methods: {
       // 获取数据列表
       getDataList () {
-        // ...
+        this.dataListLoading = true
+        const dataBody = {
+          'page': this.pageIndex,
+          'limit': this.pageSize,
+          'templatename': this.dataForm.templatename
+        }
+        metadataList(dataBody).then(({data}) => {
+          if (data && data.code === 0) {
+            this.dataList = data.page.list
+            this.totalPage = data.page.totalCount
+          } else {
+            this.dataList = []
+            this.totalPage = 0
+          }
+          this.dataListLoading = false
+        })
+        // 任务类型
+        allRuleTemplate().then(({data}) => {
+          if (data && data.code === 0) {
+            this.templateList = data.templateList
+          }
+        })
       },
       // 每页数
       sizeChangeHandle (val) {
@@ -58,11 +138,48 @@
       selectionChangeHandle (val) {
         this.dataListSelections = val
       },
+      isNull (value) {
+        return value == '' || value == null || value == undefined
+      },
       // 新增 / 修改
       addOrUpdateHandle (id) {
+        if (this.isNull(id) && this.isNull(this.dataForm.templatename)) {
+          alert('请选择模板')
+          return
+        }
         this.addOrUpdateVisible = true
         this.$nextTick(() => {
           this.$refs.addOrUpdate.init(id)
+        })
+      },
+      // 删除
+      deleteHandle (id) {
+        var ids = id ? [id] : this.dataListSelections.map(item => {
+          return item.id
+        })
+        this.$confirm(`确定对[id=${ids.join(',')}]进行[${id ? '删除' : '批量删除'}]操作?`, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$http({
+            url: this.$http.adornUrl('/canary/canaryproject/delete'),
+            method: 'post',
+            data: this.$http.adornData(ids, false)
+          }).then(({data}) => {
+            if (data && data.code === 0) {
+              this.$message({
+                message: '操作成功',
+                type: 'success',
+                duration: 1500,
+                onClose: () => {
+                  this.getDataList()
+                }
+              })
+            } else {
+              this.$message.error(data.msg)
+            }
+          })
         })
       }
     }
