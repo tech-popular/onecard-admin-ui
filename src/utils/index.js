8640396cf90b8d0b11d8f8c12402a47f3f881f9b
wanhua1993 <1126923673@qq.com>
2019-11-11 15:31:48 +0800

    sql预览优化

--- src/utils/index.js
+++ src/utils/index.js
@@ -88,3 +88,32 @@ export const getChinese = (name) => {
   names = names.trim()
   return names
 }
+
+/**
+ * @param {Number} num 数值
+ * @returns {String} 处理后的字符串
+ * @description 如果传入的数值小于10，即位数只有1位，则在前面补充0
+ */
+const getHandledValue = num => {
+  return num < 10 ? '0' + num : num
+}
+
+export const getDate = (timeStamp, startType) => {
+  const d = new Date(timeStamp)
+  const year = d.getFullYear()
+  const month = getHandledValue(d.getMonth() + 1)
+  const date = getHandledValue(d.getDate())
+  const hours = getHandledValue(d.getHours())
+  const minutes = getHandledValue(d.getMinutes())
+  const second = getHandledValue(d.getSeconds())
+  let resStr = ''
+  if (startType === 'year') resStr = year + '-' + month + '-' + date + ' ' + hours + ':' + minutes + ':' + second
+  else resStr = month + '-' + date + ' ' + hours + ':' + minutes
+  if (startType === 'month') {
+    resStr = year + '-' + month
+  }
+  if (startType === 'day') {
+    resStr = year + '-' + month + '-' + date
+  }
+  return resStr
+}
