ac1eb8e3eedd26d489eeba76c8c9cc29b87f49d4
yangxinbao <yangxinbao@ithro.com.cn>
2019-12-31 11:39:13 +0800

    指标管理模块

--- src/views/modules/dataAnalysis/indexManage.vue
+++ src/views/modules/dataAnalysis/indexManage.vue
@@ -2,24 +2,19 @@
   <div>
     <el-form :inline="true" :model="dataForm" ref="dataForm">
       <el-form-item label="指标名称">
-        <el-input v-model="dataForm.name" placeholder="指标名称" clearable />
+        <el-input v-model="dataForm.englishName" placeholder="" clearable />
       </el-form-item>
       <el-form-item label="指标类别">
-        <el-select v-model="dataForm.type" placeholder="指标类别">
-          <el-option label="全部" value="-1"></el-option>
-          <el-option label="类别一" value="1"></el-option>
-          <el-option label="类别二" value="2"></el-option>
+        <el-select v-model="dataForm.categoryId" placeholder="" filterable>
+          <el-option v-for="(item, index) in categoryIdList" :value="item.id" :key="index" :label="item.name"/>
         </el-select>
       </el-form-item>
       <el-form-item label="指标状态">
-        <el-select v-model="dataForm.status" placeholder="指标状态">
-          <el-option label="全部" value="-1"></el-option>
-          <el-option label="有效" value="1"></el-option>
-          <el-option label="无效" value="0"></el-option>
+        <el-select v-model="dataForm.enable" placeholder="指标状态">
+          <el-option label="全部" value=""></el-option>
+          <el-option label="有效" value="true"></el-option>
+          <el-option label="无效" value="false"></el-option>
         </el-select>
-      </el-form-item>
-      <el-form-item label="业务线">
-        <el-input v-model="dataForm.business" placeholder="业务线" clearable />
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="searchHandle()">查询</el-button>
@@ -29,23 +24,23 @@
     </el-form>
     <el-table :data="dataList" border v-loading="dataListLoading" style="width: 100%;">
       <el-table-column prop="id" header-align="center" align="center" label="指标ID"></el-table-column>
-      <el-table-column prop="enName" header-align="center" align="center" label="指标名"></el-table-column>
-      <el-table-column prop="name" header-align="center" align="center" label="指标标题"></el-table-column>
-      <el-table-column prop="type" header-align="center" align="center" label="指标类型"></el-table-column>
-      <el-table-column prop="classic" header-align="center" align="center" label="指标类别"></el-table-column>
-      <el-table-column prop="status" header-align="center" align="center" label="指标状态">
+      <el-table-column prop="englishName" header-align="center" align="center" label="指标名"></el-table-column>
+      <el-table-column prop="chineseName" header-align="center" align="center" label="指标标题"></el-table-column>
+      <el-table-column prop="fieldType" header-align="center" align="center" :formatter="fieldTypeFormat" label="数据类型"></el-table-column>
+      <el-table-column prop="categoryId" header-align="center" align="center" :formatter="categoryIdFormat" label="指标类别"></el-table-column>
+      <el-table-column prop="dataStandar" header-align="center" align="center" label="数据格式"></el-table-column>
+      <el-table-column prop="sourceDatasource" header-align="center" align="center" label="指标数据源"></el-table-column>
+      <el-table-column prop="remark" header-align="center" align="center" label="指标描述"></el-table-column>
+      <el-table-column prop="createUser" header-align="center" align="center" label="创建人"></el-table-column>
+      <el-table-column prop="createTime" header-align="center" align="center" label="创建时间"></el-table-column>
+      <el-table-column prop="updateTime" header-align="center" align="center" label="修改时间"></el-table-column>
+      <el-table-column prop="enable" header-align="center" align="center" label="指标状态">
         <template slot-scope="scope">
-          <el-tag v-if="scope.row.status === 1" size="small" >有效</el-tag>
+          <el-tag v-if="scope.row.enable === true" size="small" >有效</el-tag>
           <el-tag v-else size="small" type="danger">无效</el-tag>
         </template>
       </el-table-column>
-      <el-table-column prop="origin" header-align="center" align="center" label="指标数据源"></el-table-column>
-      <el-table-column prop="business" header-align="center" align="center" label="业务线"></el-table-column>
-      <el-table-column prop="desc" header-align="center" align="center" label="指标说明"></el-table-column>
-      <el-table-column prop="creator" header-align="center" align="center" label="创建人"></el-table-column>
-      <el-table-column prop="createTime" header-align="center" align="center" label="创建时间"></el-table-column>
-      <el-table-column prop="updateTime" header-align="center" align="center" label="修改时间"></el-table-column>
-      <el-table-column fixed="right" header-align="center" align="center" width="150" label="操作">
+      <el-table-column fixed="right" header-align="center" align="center" width="100" label="操作">
         <template slot-scope="scope">
           <el-button type="text" @click="addOrUpdateHandle(scope.row)">编辑</el-button>
           <el-button type="text" @click="addOrUpdateHandle(scope.row, 'view')">查看</el-button>
@@ -66,32 +61,93 @@
 </template>
 
 <script>
-  import { indexManageList } from '@/api/dataAnalysis/indexManage'
+  import { indexManageList, indexManageTypeList, indexManageMinCataList } from '@/api/dataAnalysis/indexManage'
   import AddOrUpdate from './baseComponents/indexManage-add-or-update'
   export default {
     data () {
       return {
         dataForm: {
-          name: '',
-          type: '-1',
-          status: '-1',
-          business: ''
+          englishName: '',
+          categoryId: '',
+          enable: ''
         },
         dataList: [],
         pageNum: 1, // 当前页
         pageSize: 10, // 默认每页10条
         totalCount: 0,
         dataListLoading: false,
-        addOrUpdateVisible: false
+        addOrUpdateVisible: false,
+        fieldTypeList: [ // 数据类型
+          // {
+          //   typeNum: '1',
+          //   typeValue: '类型一'
+          // },
+          // {
+          //   typeNum: '2',
+          //   typeValue: '类型2'
+          // }
+        ],
+        categoryIdList: [ // 指标类别
+          // {
+          //   typeNum: '1',
+          //   typeValue: '类别一'
+          // },
+          // {
+          //   typeNum: '2',
+          //   typeValue: '类别2'
+          // }
+        ]
       }
     },
     components: {
       AddOrUpdate
     },
     mounted () {
+      this.getCategoryIdList()
+      this.getFieldTypeList()
       this.getDataList()
     },
     methods: {
+      // 数据类型
+      fieldTypeFormat (row, column) {
+        for (var i = 0; i < this.fieldTypeList.length; i++) {
+          if (row.fieldType === this.fieldTypeList[i].childrenValue) {
+            return this.fieldTypeList[i].childrenValue
+          }
+        }
+      },
+      // 指标类别
+      categoryIdFormat (row, column) {
+        for (var i = 0; i < this.categoryIdList.length; i++) {
+          if (row.categoryId === this.categoryIdList[i].id) {
+            return this.categoryIdList[i].name
+          }
+        }
+      },
+
+      // 获取指标类别
+      getCategoryIdList () {
+        indexManageMinCataList().then(({data}) => {
+          if (data && data.status === 0) {
+            this.categoryIdList = data.dataCatalogList
+            this.categoryIdList.unshift({
+              id: '',
+              name: '全部'
+            })
+          }
+        })
+      },
+
+      // 获取数据类型
+      getFieldTypeList () {
+        let params = 6
+        indexManageTypeList(params).then(({data}) => {
+          if (data && data.status === 0) {
+            this.fieldTypeList = data.enumTypeList
+          }
+        })
+      },
+  
       // 获取数据列表
       getDataList () {
         this.$refs['dataForm'].validate((valid) => {
@@ -103,11 +159,9 @@
               'pageSize': this.pageSize
             }
             indexManageList(params, false).then(({data}) => {
-              console.log(data)
-              if (data && data.msg === 'success') {
-                this.dataList = data.data.list
-                this.totalCount = data.data.totalCount
-                console.log(this.dataList)
+              if (data && data.message === 'success') {
+                this.dataList = data.list
+                this.totalCount = data.total
               } else {
                 this.dataList = []
                 this.totalCount = 0
@@ -134,12 +188,11 @@
       resetHandle () {
         this.pageNum = 1
         this.dataForm = {
-          name: '',
-          type: '-1',
-          status: '-1',
-          business: ''
+          englishName: '',
+          categoryId: '',
+          enable: ''
         }
-        this.getDataList()
+        // this.getDataList()
       },
       // 每页数
       sizeChangeHandle (page) {
