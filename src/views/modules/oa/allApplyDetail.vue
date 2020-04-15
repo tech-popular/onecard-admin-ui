02f142b51dd39bb1fb2e82494039e39637fb80bf
lvzhiming <lvzhiming@9fbank.com.cn>
2020-04-15 16:52:45 +0800

    修改授权详情

--- src/views/modules/oa/allApplyDetail.vue
+++ src/views/modules/oa/allApplyDetail.vue
@@ -4,28 +4,7 @@
       :visible.sync="dialogVisible"
       width="50%"
       :before-close="handleClose">
-      <el-form label-width="160px">
-        <el-form-item label="授权:">
-          <el-tag
-            style="margin-left:10px;"
-            v-for="tag in checkedCities"
-            :key="tag.type"
-            :type="tag.type"
-          >{{tag.type}}</el-tag>
-        </el-form-item>
-        <el-form-item label="授权状态:">
-          <el-tag
-            style="margin-left:10px;"
-            v-for="tag in checkedCities"
-            :key="tag.flag"
-            :type="tag.flag === false ? 'danger' : 'success' "
-          >{{tag.flag}}</el-tag>
-          <!-- <span>{{checkedCities.flag}}</span> -->
-        </el-form-item>
-        <el-form-item label="失败原因:">
-          <span v-for="item in checkedCities" :key="item.desc">{{item.desc}}</span>
-        </el-form-item>
-      </el-form>
+      <p>{{checkedCities}}</p>
     </el-dialog>
 </template>
 
@@ -42,7 +21,8 @@ export default {
   methods: {
     init (val) {
       this.dialogVisible = true
-      this.checkedCities = val.grantResultJson.filter(item => item.type !== '项目空间添加用户')
+      // this.checkedCities = val.grantResultJson.filter(item => item.type !== '项目空间添加用户')
+      this.checkedCities = val.grantResultJson
     },
 
     // 弹窗状态
