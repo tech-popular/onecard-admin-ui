376d4ecccef6be742abcdf663df82f48ebb0906b
lvzhiming <lvzhiming@9fbank.com.cn>
2020-06-04 17:30:45 +0800

    取消enter提交

--- src/views/modules/canary/canaryusergroup-add-or-update.vue
+++ src/views/modules/canary/canaryusergroup-add-or-update.vue
@@ -1,6 +1,6 @@
 <template>
 <el-dialog :title="!dataForm.id ? '新增' : '修改'" :close-on-click-modal="false" :visible.sync="visible">
-  <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="100px">
+  <el-form :model="dataForm" :rules="dataRule" ref="dataForm" label-width="100px">
     <el-form-item label="用户组名称" prop="name">
       <el-input v-model="dataForm.name" placeholder="用户组名称"></el-input>
     </el-form-item>
