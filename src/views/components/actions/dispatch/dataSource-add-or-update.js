a4561ddfd3ad59afe1117d75c5d4f25f3a484066
lvzhiming <lvzhiming@9fbank.com.cn>
2020-07-22 17:02:48 +0800

    添加数据源管理

---
+++ src/views/components/actions/dispatch/dataSource-add-or-update.js
@@ -0,0 +1,118 @@
+import { saveorupt, info } from '@/api/lexicon/cacheCleanup'
+export const addOrEdotModels = {
+  data () {
+    return {
+      visible: false,
+      // 表单数据
+      formData: {},
+      // 标题字段
+      formDesc: {
+        name: {
+          type: 'input',
+          label: '数据源名称',
+          required: true
+        },
+        cacheType: {
+          type: 'select',
+          label: '数据源类型',
+          options: [
+            { text: 'redis', value: 1 },
+            { text: 'EhCahe', value: 0 }
+          ],
+          required: true
+        },
+        infos: {
+          type: 'textarea',
+          label: '数据源描述'
+        },
+        severAdis: {
+          type: 'input',
+          label: '服务器地址',
+          required: true
+        },
+        severName: {
+          type: 'input',
+          label: '数据库名称',
+          required: true
+        },
+        accountStatus: {
+          type: 'radio',
+          label: '账户类型',
+          default: 1,
+          options: [
+          { text: '公共账户', value: 1 },
+          { text: '个人帐户', value: 0 }
+          ],
+          required: true
+        },
+        userName: {
+          type: 'input',
+          label: '用户名 / Access Key ID',
+          required: true
+        },
+        password: {
+          type: 'password',
+          label: '密码 / Access Key Secret',
+          required: true
+        },
+        flag: {
+          type: 'radio',
+          label: '状态',
+          default: 1,
+          options: [
+          { text: '有效', value: 1 },
+          { text: '无效', value: 0 }
+          ],
+          required: true
+        }
+      },
+      id: '',
+      dataBody: {}
+    }
+  },
+  methods: {
+    init (id) {
+      this.id = id ? id.id : ''
+      this.visible = true
+      this.$nextTick(() => {
+        if (id) {
+          console.log(this.submitBtn)
+          this.submitBtn = false
+          const dataBody = {id: this.id}
+          info(dataBody).then(({data}) => {
+            this.formData = data.data
+          })
+        }
+      })
+    },
+    handleCancel () {
+      this.visible = false
+      this.formData = {}
+      this.id = ''
+    },
+    handleSubmit (data) {
+      this.dataBody = data
+      return Promise.resolve()
+    },
+    // 提交
+    handleSuccess () {
+      saveorupt(this.dataBody).then(({data}) => {
+        if (data && data.code === 0) {
+          this.$message({
+            message: '操作成功',
+            type: 'success',
+            duration: 1500,
+            onClose: () => {
+              this.visible = false
+              this.$emit('refreshDataList')
+              this.formData = {}
+            }
+          })
+        } else {
+          this.$message.error(data.msg)
+          this.visible = false
+        }
+      })
+    }
+  }
+}
---
+++ src/views/modules/dispatch/dataSource-add-or-update.vue
@@ -0,0 +1,18 @@
+<template>
+    <ele-form-dialog
+      v-model="formData"
+      :formDesc="formDesc"
+      :request-fn="handleSubmit"
+      :rules="rules"
+      :visible.sync="visible"
+      @request-success="handleSuccess"
+      :title="id ? '编辑' : '新增'"
+    ></ele-form-dialog>
+</template>
+
+<script>
+  import { addOrEdotModels } from '../../components/actions/dispatch/dataSource-add-or-update'
+  export default {
+    mixins: [addOrEdotModels]
+  }
+</script>
