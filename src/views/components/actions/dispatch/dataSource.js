0ef3f67af5be33001649f5d55edf7a6570dc0399
lvzhiming <lvzhiming@9fbank.com.cn>
2020-07-29 19:48:25 +0800

    提交文件

--- src/views/components/actions/dispatch/dataSource.js
+++ src/views/components/actions/dispatch/dataSource.js
@@ -1,10 +1,40 @@
-import { list, deleted } from '@/api/lexicon/cacheCleanup'
+import {
+  list,
+  deleted
+} from '@/api/dispatch/dataSource'
 export const models = {
-  data () {
-    let type = [{label: '全部', value: 1}, {label: '采集任务', value: 2}, {label: '计算任务', value: 3}, {label: '同步任务', value: 4}]
-    let typeProps = {label: 'label', value: 'value'}
-    let status = [{label: '全部', value: 1}, {label: '启用', value: 2}, {label: '停用', value: 3}]
-    let statusProps = {label: 'label', value: 'value'}
+  data() {
+    let type = [{
+      label: '全部',
+      value: 1
+    }, {
+      label: '采集任务',
+      value: 2
+    }, {
+      label: '计算任务',
+      value: 3
+    }, {
+      label: '同步任务',
+      value: 4
+    }]
+    let typeProps = {
+      label: 'label',
+      value: 'value'
+    }
+    let status = [{
+      label: '全部',
+      value: 1
+    }, {
+      label: '启用',
+      value: 2
+    }, {
+      label: '停用',
+      value: 3
+    }]
+    let statusProps = {
+      label: 'label',
+      value: 'value'
+    }
     return {
       props: {
         multiple: false,
@@ -19,8 +49,7 @@ export const models = {
       dataListLoading: false,
       addOrUpdateVisible: false,
       // 操作按钮
-      operates: [
-        {
+      operates: [{
           id: 1,
           label: '编辑',
           type: 'primary',
@@ -37,8 +66,7 @@ export const models = {
           }
         }
       ],
-      columns: [
-        {
+      columns: [{
           prop: 'id',
           label: '数据源ID',
           align: 'center'
@@ -79,38 +107,79 @@ export const models = {
           align: 'center',
           render: (h, params) => {
             return h('el-tag', {
-              props: {type: params.row.flag === 1 ? '' : 'warning'} // 组件的props
+              props: {
+                type: params.row.flag === 1 ? '' : 'warning'
+              } // 组件的props
             }, params.row.flag === 1 ? '需要' : '不需要')
           }
         }
       ],
       list: [],
-      searchData: {
-        name: null,
-        id: null,
-        type: null,
-        user: null,
-        status: null
-      },
-      searchForm: [
-        {type: 'Input', label: '数据源名称', prop: 'name', width: '300px', placeholder: '请输入名称'},
-        {type: 'Input', label: '服务器地址', prop: 'name', width: '300px', placeholder: '请输入名称'},
-        {type: 'Input', label: '数据库名称', prop: 'name', width: '300px', placeholder: '请输入名称'},
-        {type: 'Select', label: '数据源类型', prop: 'type', width: '300px', options: type, props: typeProps, change: row => '', placeholder: '请选择数据源类型'},
-        {type: 'Select', label: '状态', prop: 'status', width: '300px', options: status, props: statusProps, change: row => '', placeholder: '请选择状态'}
+      searchData: {},
+      searchForm: [{
+          type: 'Input',
+          label: '数据源名称',
+          prop: 'name',
+          width: '300px',
+          placeholder: '请输入名称'
+        },
+        {
+          type: 'Input',
+          label: '服务器地址',
+          prop: 'ip',
+          width: '300px',
+          placeholder: '请输入服务器地址'
+        },
+        {
+          type: 'Input',
+          label: '数据库名称',
+          prop: 'database',
+          width: '300px',
+          placeholder: '请输入数据库名称'
+        },
+        {
+          type: 'Select',
+          label: '数据源类型',
+          prop: 'type',
+          width: '300px',
+          options: type,
+          props: typeProps,
+          change: row => '',
+          placeholder: '请选择数据源类型'
+        },
+        {
+          type: 'Select',
+          label: '状态',
+          prop: 'status',
+          width: '300px',
+          options: status,
+          props: statusProps,
+          change: row => '',
+          placeholder: '请选择状态'
+        }
       ],
-      searchHandle: [
-        {label: '查询', type: 'primary', handle: () => { this.handleSearch() }},
-        {label: '重置', type: '', handle: () => { this.resetHandle() }},
-        {label: '新增', type: 'primary', handle: () => { this.addOrUpdateHandle() }}
+      searchHandle: [{
+          label: '查询',
+          type: 'primary',
+          handle: () => {
+            this.handleSearch()
+          }
+        },
+        {
+          label: '重置',
+          type: '',
+          handle: () => {
+            this.resetHandle()
+          }
+        }
       ]
     }
   },
-  mounted () {
+  mounted() {
     this.init()
   },
   methods: {
-    init () {
+    init() {
       const dataBody = {
         'pageNum': this.pageNum,
         'pageSize': this.pageSize,
@@ -123,37 +192,39 @@ export const models = {
       this.getList(dataBody)
     },
     // 查询
-    handleSearch () {
+    handleSearch() {
       this.pageNum = 1
       this.init()
     },
     // 重置
-    resetHandle () {
+    resetHandle() {
       this.pageNum = 1
       this.searchData = {}
       this.init()
     },
     // 新增 / 修改
-    addOrUpdateHandle (id) {
+    addOrUpdateHandle(id) {
       this.addOrUpdateVisible = true
       this.$nextTick(() => {
         this.$refs.addOrUpdate.init(id)
       })
     },
     // 每页数
-    sizeChangeHandle (val) {
+    sizeChangeHandle(val) {
       this.pageSize = val
       this.init()
     },
     // 当前页
-    currentChangeHandle (val) {
+    currentChangeHandle(val) {
       this.pageNum = val
       this.init()
     },
     // 列表接口
-    getList (dataBody) {
+    getList(dataBody) {
       this.dataListLoading = true
-      list(dataBody).then(({data}) => {
+      list(dataBody).then(({
+        data
+      }) => {
         if (data && data.code === 0) {
           this.dataListLoading = false
           this.list = data.data.list
@@ -166,14 +237,18 @@ export const models = {
       })
     },
     // 删除接口
-    deleteHandle (id) {
-      const dataBody = {'id': id.id}
+    deleteHandle(id) {
+      const dataBody = {
+        'id': id.id
+      }
       this.$confirm(`确定删除操作?`, '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
       }).then(() => {
-        deleted(dataBody).then(({data}) => {
+        deleted(dataBody).then(({
+          data
+        }) => {
           if (data && data.code === 0) {
             this.$message({
               message: '操作成功',
