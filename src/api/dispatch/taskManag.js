0b405cf5dffa86e2840aeb8bb26ae9e47c8546b0
zhangbing <zhangbing@9fbank.com.cn>
2020-08-20 10:21:27 +0800

    更新二期代码

--- src/api/dispatch/taskManag.js
+++ src/api/dispatch/taskManag.js
@@ -16,3 +16,8 @@ export const projectAll = () => httpPost(base + 'dispatch/project/selectall')
 export const sqlParse = params => httpPostFile(base + 'dispatch/task/sql/parse', params)
 export const taskBatchList = params => httpGet(base + 'dispatch/task/instance/listOnPage', params)
 export const taskExecute = id => httpPut(base + `dispatch/task/${id}/execute`) // 任务执行
+export const taskBaseInfo = id => httpGet(base + `dispatch/task/infoTask/${id}`) // 基础任务回显
+export const taskDependenceAdd = params => httpPost(base + 'dispatch/task/dependence/insertBatch', params) // 依赖任务批量新增
+export const taskBaseList = () => httpGet(base + 'dispatch/task/listOnPage') // 基础任务分页列表
+export const taskDependenceDelete = params => httpPost(base + 'dispatch/task/dependence/delete', params) // 依赖任务批量删除
+export const taskSelectDependence = id => httpGet(base + `dispatch/task/dependence/selectDependence/${id}`) // 已选依赖列表
