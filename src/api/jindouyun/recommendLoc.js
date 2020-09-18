e8cc4b4c62b216ac99d5b5d8fb91313023cbeadf
zhangbing <zhangbing@9fbank.com.cn>
2020-09-18 17:15:33 +0800

    添加同步功能

--- src/api/jindouyun/recommendLoc.js
+++ src/api/jindouyun/recommendLoc.js
@@ -2,3 +2,4 @@ import { httpGet } from '@/api'
 let base = '/'
 // 策略模块
 export const getProInfo = () => httpGet(base + 'jdy/getProInfo') // 初始化列表
+export const syncTaskInfo = () => httpGet(base + 'jdy/callUpJdyFunc') // 初始化列表
