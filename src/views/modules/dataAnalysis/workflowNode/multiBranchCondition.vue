a76fe109d2749c658b0698a2d184ad00f3f51d8d
zhangbing <zhangbing@9fbank.com.cn>
2020-09-23 17:28:57 +0800

    update

--- src/views/modules/dataAnalysis/workflowNode/multiBranchCondition.vue
+++ src/views/modules/dataAnalysis/workflowNode/multiBranchCondition.vue
@@ -165,6 +165,7 @@ import {
   dataIndexManagerCandidate
 } from '@/api/dataAnalysis/dataDecisionManage'
 import Treeselect, { LOAD_CHILDREN_OPTIONS } from '@riophae/vue-treeselect'
+import '@riophae/vue-treeselect/dist/vue-treeselect.css'
 import InputTag from '../components/InputTag'
 export default {
   data () {
