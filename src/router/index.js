ae35d4be65904499ea38490354efd1b445d53136
zhangbing <zhangbing@9fbank.com.cn>
2020-10-19 11:19:09 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,7 @@ const globalRoutes = [
   { path: '/login', component: _import('common/login'), name: 'login', meta: { title: '登录' } },
   { path: '/home', component: _import('common/home'), name: 'home', meta: { title: '首页' } },
   { path: '/resetPassword', component: _import('common/resetPass'), name: 'resetPass', meta: { title: '重置密码' } },
-  { path: '', redirect: '/login' }
+  { path: '/canary', redirect: '/login' }
 ]
 
 // 主入口路由(需嵌套上左右整体布局)
