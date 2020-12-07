b3397cb7248e95fbd4af72768c15979150ab3111
张亚男 <zhangyanan>
2020-12-07 18:38:38 +0800

    查看日志

--- src/views/modules/dispatch/taskBranch-view-log.vue
+++ src/views/modules/dispatch/taskBranch-view-log.vue
@@ -1,6 +1,7 @@
 <template>
-  <el-dialog title='查看日志' :append-to-body="true" :close-on-click-modal="false" :visible.sync="visible">
-  <div style="width:100%;height:500px;overflow:auto;word-wrap: break-word;word-break: normal;background:black;color:#fff;" v-html="logText"></div>
+  <el-dialog title='查看日志' :fullscreen="true" :append-to-body="true" :close-on-click-modal="false" :visible.sync="visible">
+  <div style="width:100%; height:600px;overflow:auto;word-wrap:font-size:12px; break-word;word-break: normal;background:black;color:#fff;" v-html="logText">
+  </div>
     <div slot="footer" class="foot">
       <el-button @click="visible = false">取消</el-button>
     </div>	 
