a312db742048c99fd8fde08c061cd12b48237f01
张亚男 <zhangyanan>
2020-12-08 16:23:04 +0800

    日志增加放大缩小功能

--- src/views/modules/dispatch/taskBranch-view-log.vue
+++ src/views/modules/dispatch/taskBranch-view-log.vue
@@ -1,6 +1,13 @@
 <template>
-  <el-dialog title='查看日志' :fullscreen="true" :append-to-body="true" :close-on-click-modal="false" :visible.sync="visible">
-  <div style="width:100%; height:600px;overflow:auto;padding-left:15px;word-wrap:break-word;word-break: normal;background:black;color:#fff;" v-html="logText">
+  <el-dialog title='查看日志' :fullscreen="isFullscreen" :show-close="false" :append-to-body="true" :close-on-click-modal="false" :visible.sync="visible" class="view-log">
+  <div slot="title" class="view-log-title">
+    <div>查看日志</div>
+    <div>
+      <el-button size="small" @click="isFullscreen = !isFullscreen"> {{isFullscreen ? '缩小' : '放大'}}</el-button>
+      <el-button size="small" @click="visible = false">关闭</el-button>
+    </div>
+  </div>
+  <div style="width:100%; height:600px; overflow:auto; padding-left:15px; word-wrap:break-word; word-break: normal; background:black;color:#fff;" v-html="logText">
   </div>
     <!-- <div slot="footer" class="foot">
       <el-button @click="visible = false">取消</el-button>
@@ -13,6 +20,7 @@ export default{
   data () {
     return {
       visible: false,
+      isFullscreen: false,
       logText: ''
     }
   },
@@ -31,3 +39,15 @@ export default{
   }
 }
 </script>
+<style>
+ .view-log-title{
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+  /* padding-right:50px; */
+  font-size: 16px;
+}
+.view-log-title .el-dialog__headerbtn {
+  font-size: 20px;
+}
+</style>
