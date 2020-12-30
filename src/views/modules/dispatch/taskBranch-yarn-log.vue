5ceb4c6e50db5bd9ddf13126a023c85089b79318
张亚男 <zhangyanan>
2020-12-30 10:36:48 +0800

    update

--- src/views/modules/dispatch/taskBranch-yarn-log.vue
+++ src/views/modules/dispatch/taskBranch-yarn-log.vue
@@ -1,25 +1,13 @@
 <template>
   <el-dialog title='Yarn日志查询' :fullscreen="isFullscreen"  :append-to-body="true" :close-on-click-modal="false" :visible.sync="visible" class="yarn-log">
-  <!-- <div slot="title" class="view-log-title">
-    <div>查看Yarn日志</div> -->
-    <!-- <div>
-      <el-button size="small" @click="isFullscreen = !isFullscreen"> {{isFullscreen ? '缩小' : '放大'}}</el-button>
-      <el-button size="small" @click="visible = false">关闭</el-button>
-    </div> -->
-  <!-- </div> -->
   <el-form :model="dataForm" ref="dataForm">
 		<el-form-item label="applicationId ：" label-width="150px">
       <el-input placeholder="请输入applicationId" v-model="dataForm.applicationId" class="yarn-log-input"></el-input>
 			<el-button @click="handleSearch()">查询</el-button>
 		</el-form-item>
-		<el-form-item>
-			<div v-if="logTextisible" style="width:100%; height:600px; overflow:auto; padding-left:15px; word-wrap:break-word; word-break: normal; background:black;color:#fff;" v-html="logText">
-      </div>
-		</el-form-item>
 	</el-form>
-  <!-- <div slot="footer" class="foot">
-      <el-button @click="visible = false">取消</el-button>
-    </div>	  --> 
+  	<div v-if="logTextisible" style="width:100%; height:600px; overflow:auto; padding-left:15px; word-wrap:break-word; word-break: normal; background:black;color:#fff;" v-html="logText">
+    </div>
 	</el-dialog>
 </template>
 <script>
@@ -40,11 +28,11 @@ export default{
     init () {
       this.isFullscreen = false
       this.logTextisible = false
-      this.visible = true
+      this.dataForm = {
+        applicationId: ''
+      }
       this.logText = ''
-      this.$nextTick(() => {
-        this.$refs['dataForm'].resetFields()
-      })
+      this.visible = true
     },
     handleSearch () { // 提交查询
       if (!this.dataForm.applicationId) {
@@ -58,7 +46,8 @@ export default{
               duration: 1500,
               onClose: () => {
                 this.logTextisible = true
-                 this.logText = data.data && data.data.replace(/\n/g, '<br>')
+                this.isFullscreen = true
+                this.logText = data.data && data.data.replace(/\n/g, '<br>')
               }
             })
           } else {
