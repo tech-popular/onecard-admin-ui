aef31d893f2bf6da2cb4ad41b967e83993b56ef8
张亚男 <zhangyanan>
2021-01-18 18:37:21 +0800

    依赖快照

--- src/views/modules/dispatch/taskManag-snap-shot.vue
+++ src/views/modules/dispatch/taskManag-snap-shot.vue
@@ -4,7 +4,7 @@
 		:fullscreen="true"
 	  title="依赖快照"
     :close-on-click-modal="false">
-  <el-tabs v-model="editableTabsValue" type="border-card" closable @tab-remove="removeTab">
+  <el-tabs v-model="editableTabsValue" type="border-card" @tab-click="tabClick">
   <el-tab-pane
     v-for="(item, index) in editableTabs"
     :key="index"
@@ -12,130 +12,18 @@
     :name="item.name"
 		:stretch = true
   >
-	<el-tabs v-model="item.content.activeName">
+	<el-tabs v-model="item.content.activeName"  @tab-click="tabhandleClick">
     <el-tab-pane label="上游任务" name="first">
-			<el-table
-			  :data="item.content.tableList"
-				border
-				width="100%"
-			>
-			 <el-table-column
-        prop="id"
-        header-align="center"
-        align="center"
-        label="任务名称">
-      </el-table-column>
-			<el-table-column
-        prop="name"
-        header-align="center"
-        align="center"
-        label="系统名称">
-      </el-table-column>
-			<el-table-column
-        prop="type"
-        header-align="center"
-        align="center"
-        label="任务类型">
-      </el-table-column>
-			<el-table-column
-        prop="person"
-        header-align="center"
-        align="center"
-        label="负责人">
-      </el-table-column>
-			<el-table-column
-        prop="time"
-        header-align="center"
-        align="center"
-        label="时间触发">
-      </el-table-column>
-			<el-table-column
-        prop="ishas"
-        header-align="center"
-        align="center"
-        label="有无依赖">
-      </el-table-column>
-			<el-table-column
-        prop="this"
-        header-align="center"
-        align="center"
-        label="本次执行时间">
-      </el-table-column>
-			<el-table-column
-        prop="wancheng"
-        header-align="center"
-        align="center"
-        label="本次完成时间">
-      </el-table-column>
-			<el-table-column
-        prop="next"
-        header-align="center"
-        align="center"
-        label="下次执行时间">
-      </el-table-column>
-			</el-table>
+			<h3>新调度任务依赖</h3>
+			<taskManageSnapShotTable></taskManageSnapShotTable>
+			<h3>老调度任务依赖</h3>
+			<taskManageSnapShotTable></taskManageSnapShotTable>
 		</el-tab-pane>
     <el-tab-pane label="下游任务" name="second">
-			<el-table
-			  :data="item.content.list"
-				border
-				width="100%"
-			>
-			 <el-table-column
-        prop="id"
-        header-align="center"
-        align="center"
-        label="任务名称">
-      </el-table-column>
-			<el-table-column
-        prop="name"
-        header-align="center"
-        align="center"
-        label="系统名称">
-      </el-table-column>
-			<el-table-column
-        prop="type"
-        header-align="center"
-        align="center"
-        label="任务类型">
-      </el-table-column>
-			<el-table-column
-        prop="person"
-        header-align="center"
-        align="center"
-        label="负责人">
-      </el-table-column>
-			<el-table-column
-        prop="time"
-        header-align="center"
-        align="center"
-        label="时间触发">
-      </el-table-column>
-			<el-table-column
-        prop="ishas"
-        header-align="center"
-        align="center"
-        label="有无依赖">
-      </el-table-column>
-			<el-table-column
-        prop="this"
-        header-align="center"
-        align="center"
-        label="本次执行时间">
-      </el-table-column>
-			<el-table-column
-        prop="wancheng"
-        header-align="center"
-        align="center"
-        label="本次完成时间">
-      </el-table-column>
-			<!-- <el-table-column
-        prop="next"
-        header-align="center"
-        align="center"
-        label="下次执行时间">
-      </el-table-column> -->
-			</el-table>
+			<h3>新调度任务依赖</h3>
+			<taskManageSnapShotTable></taskManageSnapShotTable>
+			<h3>老调度任务依赖</h3>
+			<taskManageSnapShotTable></taskManageSnapShotTable>
 		</el-tab-pane>
   </el-tabs>
   </el-tab-pane>
@@ -144,52 +32,49 @@
 </template>
 
 <script>
+import taskManageSnapShotTable from './taskManage-snapShot-table.vue'
 export default {
   data () {
     return {
       visible: false,
       editableTabsValue: '1',
       editableTabs: [{
-        title: 'Tab 1',
+        title: '',
         name: '1',
         content: {
-          tableList: [],
-          list: [],
-          activeName: 'second'
+          activeName: 'first'
         }}],
         tabIndex: 1
     }
   },
+  components: { taskManageSnapShotTable },
   methods: {
     init (row) {
       console.log('row: ', row)
-      this.editableTabs = row.name
+      this.editableTabs[0].title = row.taskName
       this.visible = true
     },
+    // 点击依赖快照
     addTab(targetName) {
         let newTabName = ++this.tabIndex + ''
         this.editableTabs.push({
           title: 'New Tab',
           name: newTabName,
-          content: 'New Tab content'
+          content: {
+            activeName: 'first'
+          }
         })
         this.editableTabsValue = newTabName
       },
-      removeTab(targetName) {
-        let tabs = this.editableTabs
-        let activeName = this.editableTabsValue
-        if (activeName === targetName) {
-          tabs.forEach((tab, index) => {
-            if (tab.name === targetName) {
-              let nextTab = tabs[index + 1] || tabs[index - 1]
-              if (nextTab) {
-                activeName = nextTab.name
-              }
-            }
-          })
-        }
-        this.editableTabsValue = activeName
-        this.editableTabs = tabs.filter(tab => tab.name !== targetName)
+      // 切换任务
+      tabClick (tab, event) {
+        // console.log('event: ', event)
+        // console.log('tab: ', tab)
+      },
+      // 切换上下游
+      tabhandleClick (tab, event) {
+        console.log('event: ', event)
+        console.log('tab: ', tab)
       }
   }
 }
---
+++ src/views/modules/dispatch/taskManage-snapShot-table.vue
@@ -0,0 +1,98 @@
+<template>
+<div>
+	<el-table :data="list" border	width="100%">
+			 <el-table-column  prop="id"  header-align="center" align="center" label="任务名称"></el-table-column>
+			 <el-table-column  prop="name"  header-align="center" align="center" label="系统名称"></el-table-column>
+			 <el-table-column  prop="type"  header-align="center"  align="center"  label="任务类型"></el-table-column>
+			 <el-table-column  prop="person"  header-align="center" align="center"  label="负责人"></el-table-column>
+			 <el-table-column
+        prop="time"
+        header-align="center"
+        align="center"
+        label="时间触发">
+      </el-table-column>
+			<el-table-column
+        prop="ishas"
+        header-align="center"
+        align="center"
+        label="有无依赖">
+      </el-table-column>
+			<el-table-column
+        prop="this"
+        header-align="center"
+        align="center"
+        label="本次执行时间">
+      </el-table-column>
+			<el-table-column
+        prop="wancheng"
+        header-align="center"
+        align="center"
+        label="本次完成时间">
+      </el-table-column>
+			<el-table-column
+        prop="next"
+        header-align="center"
+        align="center"
+        label="下次执行时间">
+      </el-table-column>
+			 <el-table-column
+        header-align="center"
+        align="center"
+        width="150"
+        label="操作">
+        <template slot-scope="scope">
+          <el-button type="text" size="small" @click="addOrUpdateHandle(scope.row)">依赖快照</el-button>
+        </template>
+      </el-table-column>
+			</el-table>
+			<el-pagination
+      @size-change="sizeChangeHandle"
+      @current-change="currentChangeHandle"
+      :current-page="pageIndex"
+      :page-sizes="[10, 20, 50, 100]"
+      :page-size="pageSize"
+      :total="totalPage"
+      layout="total, sizes, prev, pager, next, jumper"
+    ></el-pagination>
+</div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      list: [],
+      id: '',
+      pageIndex: 1,
+      pageSize: 10,
+      totalPage: 0
+    }
+  },
+  methods: {
+    init () {
+      this.pageIndex = 1
+      this.pageSize = 10
+      this.totalPage = 0
+      this.getDataList()
+    },
+    getDataList () {
+      console.log('11111')
+    },
+    // 每页数
+    sizeChangeHandle (val) {
+      this.pageSize = val
+      this.pageIndex = 1
+      this.getDataList()
+    },
+    // 当前页
+    currentChangeHandle (val) {
+      this.pageIndex = val
+      this.getDataList()
+    },
+    // 点击查看快照
+    addOrUpdateHandle (row) {
+      console.log('row: ', row)
+      this.$emit('addTab', row)
+    }
+  }
+}
+</script>
