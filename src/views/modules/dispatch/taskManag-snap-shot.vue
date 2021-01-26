18872b9515199bfda5bb96116e05d6038dee9912
张亚男 <zhangyanan>
2021-01-26 16:55:14 +0800

    依赖快照添加删除功能

--- src/views/modules/dispatch/taskManag-snap-shot.vue
+++ src/views/modules/dispatch/taskManag-snap-shot.vue
@@ -4,10 +4,10 @@
 		:fullscreen="true"
 	  title="依赖快照"
     :close-on-click-modal="false">
-  <el-tabs v-model="editableTabsValue" type="border-card" @tab-click="tabClick">
+  <el-tabs v-model="editableTabsValue" type="border-card" @tab-click="tabClick" :closable ="closable" @tab-remove="removeTab">
   <el-tab-pane
-    v-for="(item, index) in editableTabs"
-    :key="index"
+    v-for="(item) in editableTabs"
+    :key="item.name"
     :label="item.title"
     :name="item.name"
 		:stretch = true
@@ -40,14 +40,15 @@ export default {
       visible: false,
       taskId: '',
       editableTabsValue: '1',
-      taskIdLIst: [],
+      closable: false,
       taskSnapshotNewDepends: taskSnapshotNewDepends,
       taskSnapshotOldDepends: taskSnapshotOldDepends,
       editableTabs: [{
         title: '',
         name: '1',
         content: {
-          activeName: 'first'
+          activeName: 'first',
+          id: ''
         }}],
         tabIndex: 1,
         tabClickIndex: 0
@@ -56,18 +57,17 @@ export default {
   components: { taskManageSnapShotTable },
   methods: {
     init (row) {
-      this.taskIdLIst = []
       this.editableTabs = [{
         title: row.taskName,
         name: '1',
         content: {
-          activeName: 'first'
+          activeName: 'first',
+          id: row.id
         }
       }]
       this.editableTabsValue = '1'
       this.tabIndex = 1
       this.taskId = row.id
-      this.taskIdLIst.push(row.id)
       this.visible = true
       this.$nextTick(() => {
         // 上游任务
@@ -82,13 +82,14 @@ export default {
           title: targetName.taskName,
           name: newTabName,
           content: {
-            activeName: 'first'
+            activeName: 'first',
+            id: targetName.taskId
           }
         })
-        this.taskIdLIst.push(targetName.taskId)
         this.taskId = targetName.taskId
-        this.tabClickIndex = this.tabIndex - 1
+        this.closable = true
         this.$nextTick(() => {
+          this.tabClickIndex = this.editableTabs.length - 1
           this.$refs.taskSnapshotUpNewDepends[this.tabClickIndex].init()
           this.$refs.taskSnapshotUpOldDepends[this.tabClickIndex].init()
         })
@@ -97,15 +98,44 @@ export default {
       // 切换任务
       tabClick (tab, event) {
         this.tabClickIndex = tab.index
-        this.taskId = this.taskIdLIst[this.tabClickIndex]
+        this.taskId = this.editableTabs[this.tabClickIndex].content.id
         this.editableTabs[this.tabClickIndex].content.activeName = 'first'
         this.$nextTick(() => {
           this.$refs.taskSnapshotUpNewDepends[this.tabClickIndex].init()
           this.$refs.taskSnapshotUpOldDepends[this.tabClickIndex].init()
         })
       },
+      // 删除任务
+      removeTab(targetName) {
+        this.closable = true
+        let tabs = this.editableTabs
+        let activeName = this.editableTabsValue
+        if (activeName === targetName) {
+          tabs.forEach((tab, index) => {
+            if (tab.name === targetName) {
+              let nextTab = tabs[index + 1] || tabs[index - 1]
+              if (nextTab) {
+                activeName = nextTab.name
+              }
+            }
+          })
+        }
+        this.editableTabsValue = activeName
+        this.editableTabs = tabs.filter(tab => tab.name !== targetName)
+        tabs.map((tab, index) => {
+          if (tab.name === targetName) {
+            if (index < this.tabClickIndex) {
+              this.tabClickIndex = this.tabClickIndex - 1
+            }
+          }
+        })
+        if (this.editableTabs.length === 1) {
+          this.closable = false
+        }
+      },
       // 切换上下游
       tabhandleClick (tab, event) {
+        console.log('上下 ', this.tabClickIndex)
         if (tab.index === '0') {
           this.$nextTick(() => {
             this.$refs.taskSnapshotUpNewDepends[this.tabClickIndex].init()
