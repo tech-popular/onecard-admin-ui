1a17b5e849915f761524251850267d1d540d1dab
张亚男 <zhangyanan>
2021-12-08 16:17:17 +0800

    update

--- src/views/modules/BI-Manager/menu-add-or-update.vue
+++ src/views/modules/BI-Manager/menu-add-or-update.vue
@@ -228,7 +228,7 @@
               'name': this.dataForm.name,
               'url': this.dataForm.type === 0 ? '' : this.dataForm.url,
               'taskIds': this.dataForm.type === 0 ? '' : this.dataForm.taskIds.join(';'),
-              'menuParentList': this.dataForm.type === 0 ? '' : this.menuParentList.join(',')
+              'menuParentList': this.menuParentList.join(',')
             }
               console.log('params: ', params)
             if (!this.dataForm.id) {
