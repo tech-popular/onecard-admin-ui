4f3b346fb31860df51fe2a996c746743e955617d
peigang <peigang@ethstock.cc>
2022-01-12 11:08:18 +0800

    用户组修改字段

--- src/api/BI-Manager/userGroup.js
+++ src/api/BI-Manager/userGroup.js
@@ -5,7 +5,7 @@ export const savaUserGroupInfo = params => httpPost(da + 'bi/biUserGroup/save', 
 export const updateUserGroupInfo = params => httpPost(da + 'bi/biUserGroup/update', params) // 编辑保存
 export const lookDataInfo = id => httpGet(da + `bi/biUserGroup/info/${id}`) // 根据id查询详情
 export const getSelectTenantDown = () => httpPost(da + 'tenantManage/selectTenantDown') // 租户列表
-export const getUsersList = tenantId => httpGet(da + `oa/getUsersFromTenantId/${tenantId}`) // 用户列表
+export const getUsersList = params => httpGet(da + `sys/user/getUserListByName`, params) // 用户列表
 export const deleteUsersById = id => httpGet(da + `bi/biUserGroup/deleteById/${id}`) // 删除
 export const getRoleMenuList = userGroupId => httpGet(da + 'bi/biSysUserGroupMenu/getRoleMenuList', userGroupId) // 获取授权菜单id集合
 export const saveRoleInfo = params => httpPost(da + 'bi/biSysUserGroupMenu/saveRole', params) // 分配权限
