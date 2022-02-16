57fb539deb626a1931e7abcf4fc92f2b1a89f2ab
张亚男 <zhangyanan>
2022-02-16 16:31:50 +0800

    数据导入

---
+++ src/views/modules/withdrawal/dataImport.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="mod-config">
+    <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
+      <el-form-item label="上传人: ">
+        <el-input v-model="dataForm.roleName" placeholder="角色名称" clearable></el-input>
+      </el-form-item>
+      <el-form-item label="上传时间: ">
+        <el-input v-model="dataForm.roleName" placeholder="角色名称" clearable></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="getDataList()">上传数据</el-button>
+        <el-button @click="resetHandle">下载模板</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table
+      :data="dataList"
+      border
+      v-loading="dataListLoading"
+      style="width: 100%;"
+    >
+      <el-table-column prop="id" header-align="center" align="center" label="序号"></el-table-column>
+      <el-table-column prop="userName" header-align="center" align="center" label="上传人"></el-table-column>
+      <el-table-column prop="startTime" header-align="center" align="center" show-overflow-tooltip label="开始时间"></el-table-column>
+      <el-table-column prop="endTime" header-align="center" align="center" label="结束时间"></el-table-column>
+			<el-table-column prop="createTime" header-align="center" align="center" label="上传时间"></el-table-column>
+    </el-table>
+    <el-pagination
+      @size-change="sizeChangeHandle"
+      @current-change="currentChangeHandle"
+      :current-page="pageIndex"
+      :page-sizes="[10, 20, 50, 100]"
+      :page-size="pageSize"
+      :total="totalPage"
+      layout="total, sizes, prev, pager, next, jumper"
+    ></el-pagination>
+  </div>
+</template>
+<style>
+.input-with-select {
+  width: 180px;
+}
+</style>
+<script>
+export default {
+  data () {
+    return {
+      restaurants: [],
+      dataList: [],
+      pageIndex: 1,
+      pageSize: 10,
+      totalPage: 0,
+      dataListLoading: false
+    }
+  },
+  activated () {
+    this.getDataList()
+  },
+  mounted () {
+  },
+  methods: {
+    // 获取数据列表
+    getDataList () {
+      // this.dataListLoading = true
+    },
+    // 每页数
+    sizeChangeHandle (val) {
+      this.pageSize = val
+      this.pageIndex = 1
+      this.getDataList()
+    },
+    // 当前页
+    currentChangeHandle (val) {
+      this.pageIndex = val
+      this.getDataList()
+    }
+  }
+}
+</script>
