28a7ff47141b2b0f55562f794782813b301fbf79
张亚男 <zhangyanan>
2022-08-05 14:59:08 +0800

    update

--- src/api/dataGovernance/subscribeManage.js
+++ src/api/dataGovernance/subscribeManage.js
@@ -1,4 +1,4 @@
-import { httpPost, httpGet } from '@/api'
+import { httpGet } from '@/api'
 let da = '/'
 export const queryEnableList = params => httpGet(da + 'data/governance/datasource/queryEnableList', params) // 查询所有启用数据源列表
 export const databaseList = datasourceId => httpGet(da + `data/governance/database/${datasourceId}`) // 查看数据源下所有库
