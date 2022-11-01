711084200651336dd817401871367b8d133ac98f
薛渊远 <hsuehyy@foxmail.com>
2022-11-01 15:17:33 +0800

    联合登陆bi系统

--- static/config/index-qa.js
+++ static/config/index-qa.js
@@ -9,6 +9,8 @@
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain'] = './' // 域名
+  window.SITE_CONFIG['BIGDATA_HREFLINK'] = 'http://test02.bigdata.9f.cn' // 域名
+
   window.SITE_CONFIG['version'] = ''   // 版本号(年月日时分)
   window.SITE_CONFIG['cdnUrl'] = window.SITE_CONFIG.domain + window.SITE_CONFIG.version
 })()
--- static/config/index-uat.js
+++ static/config/index-uat.js
@@ -9,6 +9,8 @@
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain'] = './' // 域名
+  window.SITE_CONFIG['BIGDATA_HREFLINK'] = '//test.tech.9fbank.com' // 域名
+
   window.SITE_CONFIG['version'] = ''   // 版本号(年月日时分)
   window.SITE_CONFIG['cdnUrl'] = window.SITE_CONFIG.domain + window.SITE_CONFIG.version
 })()
