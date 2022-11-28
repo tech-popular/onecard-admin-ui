2bf164cb5bbdac6e36285ddac2813a7a5d1a3ed2
Hsueh <hsuehyy@foxmail.com>
2022-11-28 15:23:52 +0800

    feat：增加查看历史sql

--- src/views/modules/dataGovernance/subscribeManage.vue
+++ src/views/modules/dataGovernance/subscribeManage.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="mod-config">
     <el-form :inline="true" :model="dataForm">
-      <el-form-item label="申请原因: ">
-        <el-input v-model="dataForm.approveReason" clearable  class="input-with-select" @keyup.enter.native="getDataList()"></el-input>
+      <el-form-item label="ID: ">
+        <el-input
+          v-model="dataForm.id"
+          clearable
+          class="input-with-select"
+          @keyup.enter.native="getDataList()"
+        ></el-input>
       </el-form-item>
-     <el-form-item label="类型: ">
+      <el-form-item label="申请原因: ">
+        <el-input
+          v-model="dataForm.approveReason"
+          clearable
+          class="input-with-select"
+          @keyup.enter.native="getDataList()"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="类型: ">
         <el-select v-model="dataForm.type" clearable>
           <el-option label="SQL提数提数" value="0"></el-option>
           <el-option label="订阅" value="1"></el-option>
@@ -18,28 +31,102 @@
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="searchData()">查询</el-button>
-        <el-button type="primary" v-if="isAdmin" @click="multiTaskPermission()">批量授权</el-button>
+        <el-button type="primary" v-if="isAdmin" @click="multiTaskPermission()"
+          >批量授权</el-button
+        >
       </el-form-item>
     </el-form>
-    <el-table :data="dataList" border v-loading="dataListLoading" @selection-change="selectionChangeHandle" style="width: 100%;">
-      <el-table-column v-if="isAdmin" type="selection" header-align="center" align="center" width="50"></el-table-column>
-      <el-table-column prop="id" header-align="center" align="center" label="ID"></el-table-column>
-       <el-table-column prop="typeDesc" header-align="center" align="center" label="类型"></el-table-column>
-      <el-table-column prop="approveReason" header-align="center" align="center" label="申请原因"></el-table-column>
-      <el-table-column prop="receiveTypeDesc" header-align="center" align="center" label="接收方式"></el-table-column>
-      <el-table-column prop="receiveContentTypeDesc" header-align="center" align="center" label="接收内容类型"></el-table-column>
-      <el-table-column prop="exportTypeDesc" header-align="center" align="center" label="订阅周期"></el-table-column>
-       <el-table-column prop="receiveDays" header-align="center" align="center" label="接收天设置"></el-table-column>
-      <el-table-column prop="createTime" header-align="center" align="center" label="申请时间"></el-table-column>
-      <el-table-column prop="createUser" header-align="center" align="center" label="申请人"></el-table-column>
-      <el-table-column prop="expiryTime" header-align="center" align="center" label="过期时间"></el-table-column>
-      <el-table-column prop="status" header-align="center" align="center" label="上线状态">
+    <el-table
+      :data="dataList"
+      border
+      v-loading="dataListLoading"
+      @selection-change="selectionChangeHandle"
+      style="width: 100%"
+    >
+      <el-table-column
+        v-if="isAdmin"
+        type="selection"
+        header-align="center"
+        align="center"
+        width="50"
+      ></el-table-column>
+      <el-table-column
+        prop="id"
+        header-align="center"
+        align="center"
+        label="ID"
+      ></el-table-column>
+      <el-table-column
+        prop="typeDesc"
+        header-align="center"
+        align="center"
+        label="类型"
+      ></el-table-column>
+      <el-table-column
+        prop="approveReason"
+        header-align="center"
+        align="center"
+        label="申请原因"
+      ></el-table-column>
+      <el-table-column
+        prop="receiveTypeDesc"
+        header-align="center"
+        align="center"
+        label="接收方式"
+      ></el-table-column>
+      <el-table-column
+        prop="receiveContentTypeDesc"
+        header-align="center"
+        align="center"
+        label="接收内容类型"
+      ></el-table-column>
+      <el-table-column
+        prop="exportTypeDesc"
+        header-align="center"
+        align="center"
+        label="订阅周期"
+      ></el-table-column>
+      <el-table-column
+        prop="receiveDays"
+        header-align="center"
+        align="center"
+        label="接收天设置"
+      ></el-table-column>
+      <el-table-column
+        prop="createTime"
+        header-align="center"
+        align="center"
+        label="申请时间"
+      ></el-table-column>
+      <el-table-column
+        prop="createUser"
+        header-align="center"
+        align="center"
+        label="申请人"
+      ></el-table-column>
+      <el-table-column
+        prop="expiryTime"
+        header-align="center"
+        align="center"
+        label="过期时间"
+      ></el-table-column>
+      <el-table-column
+        prop="status"
+        header-align="center"
+        align="center"
+        label="上线状态"
+      >
         <template slot-scope="scope">
           <el-tag v-if="scope.row.status === 0" size="small">未上线</el-tag>
           <el-tag v-if="scope.row.status === 1" size="small">已上线</el-tag>
         </template>
       </el-table-column>
-      <el-table-column prop="approveStatus" header-align="center" align="center" label="审批状态">
+      <el-table-column
+        prop="approveStatus"
+        header-align="center"
+        align="center"
+        label="审批状态"
+      >
         <!-- <template slot-scope="scope">
           <el-tag v-if="scope.row.approveStatus === 0" size="small" type="danger">审批异常</el-tag>
           <el-tag v-if="scope.row.approveStatus === 1" size="small">敏感数据验证审批中</el-tag>
@@ -47,13 +134,63 @@
           <el-tag v-if="scope.row.approveStatus === 3" size="small">审批完成</el-tag>
         </template> -->
       </el-table-column>
-      <el-table-column header-align="center" align="center" width="150" label="操作">
+      <el-table-column
+        header-align="center"
+        align="center"
+        width="150"
+        label="操作"
+      >
         <template slot-scope="scope">
-          <el-button type="text" size="small" v-if=" authHandle(scope.row) && scope.row.type === 0" @click="addOrUpdateHandle(scope.row.id)">查看</el-button>
-          <el-button type="text" size="small" v-if=" authHandle(scope.row) && scope.row.type === 0 && scope.row.status === 0" @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
-          <el-button type="text" size="small" @click="dispatchConfig(scope.row)" v-if="scope.row.type === 0 && scope.row.exportType === 'period' && scope.row.status === 0">配置调度</el-button>
-          <el-button type="text" size="small" v-if="authHandle(scope.row) && scope.row.type === 0 " @click="upOrDownHandle(scope.row)">{{scope.row.status === 0 ? '上线' : '下线'}}</el-button>
-          <el-button type="text" size="small" v-if="isAdmin || scope.row.authOwner === userid || scope.row.authOwner === username" @click="taskPermission(scope.row)">授权</el-button>
+          <el-button
+            type="text"
+            size="small"
+            v-if="authHandle(scope.row) && scope.row.type === 0"
+            @click="addOrUpdateHandle(scope.row.id)"
+            >查看</el-button
+          >
+          <el-button
+            type="text"
+            size="small"
+            v-if="
+              authHandle(scope.row) &&
+              scope.row.type === 0 &&
+              scope.row.status === 0
+            "
+            @click="addOrUpdateHandle(scope.row.id)"
+            >修改</el-button
+          >
+          <el-button
+            type="text"
+            size="small"
+            @click="dispatchConfig(scope.row)"
+            v-if="
+              scope.row.type === 0 &&
+              scope.row.exportType === 'period' &&
+              scope.row.status === 0
+            "
+            >配置调度</el-button
+          >
+          <el-button
+            type="text"
+            size="small"
+            v-if="authHandle(scope.row) && scope.row.type === 0"
+            @click="upOrDownHandle(scope.row)"
+            >{{ scope.row.status === 0 ? "上线" : "下线" }}</el-button
+          >
+          <el-button
+            type="text"
+            size="small"
+            v-if="
+              isAdmin ||
+              scope.row.authOwner === userid ||
+              scope.row.authOwner === username
+            "
+            @click="taskPermission(scope.row)"
+            >授权</el-button
+          >
+          <el-button type="text" size="small" @click="historySql(scope.row)"
+            >历史SQL</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
@@ -66,10 +203,37 @@
       :total="totalPage"
       layout="total, sizes, prev, pager, next, jumper"
     ></el-pagination>
-    <assign-permission v-if="assignPermissionVisible" :submitDataApi="submitDataApi" :submitDataApis="submitDataApis" ref="assignPermission" @refreshDataList="getDataList"></assign-permission>
+    <assign-permission
+      v-if="assignPermissionVisible"
+      :submitDataApi="submitDataApi"
+      :submitDataApis="submitDataApis"
+      ref="assignPermission"
+      @refreshDataList="getDataList"
+    ></assign-permission>
     <!--调度配置-->
-    <dispatch-config-add-or-update v-if="dispatchConfigAddOrUpdateVisible" ref="dispatchConfigAddOrUpdate" @refreshDataList="getDataList" />
-    <addOrUpdate v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></addOrUpdate>
+    <dispatch-config-add-or-update
+      v-if="dispatchConfigAddOrUpdateVisible"
+      ref="dispatchConfigAddOrUpdate"
+      @refreshDataList="getDataList"
+    />
+    <addOrUpdate
+      v-if="addOrUpdateVisible"
+      ref="addOrUpdate"
+      @refreshDataList="getDataList"
+    ></addOrUpdate>
+
+    <!-- Table -->
+    <el-dialog title="历史sql列表" :visible.sync="dialogTableVisible">
+      <el-table :data="historySqlList">
+        <el-table-column property="id" label="ID"  width="60"></el-table-column>
+        <el-table-column property="sqlTitle" label="sql标题" width="120"></el-table-column>
+        <el-table-column property="sql" label="sql语句" width="300"></el-table-column>
+        <el-table-column property="dataJobId" label="订阅任务ID"  width="120"></el-table-column>
+        <el-table-column property="sqlBatchId" label="sql批次ID" width="120"></el-table-column>
+        <el-table-column property="datasourceId" label="数据源ID"></el-table-column>
+        <el-table-column property="databaseId" label="数据库ID"></el-table-column>
+      </el-table>
+    </el-dialog>
   </div>
 </template>
 <style>
@@ -78,13 +242,15 @@
 }
 </style>
 <script>
-import { getList, subscriptionUpAndDown, updateSubscriptionAuth, batchUpdateSubscriptionAuth } from '@/api/dataGovernance/subscribeManage'
+import { getList, subscriptionUpAndDown, watchHistorySql, updateSubscriptionAuth, batchUpdateSubscriptionAuth } from '@/api/dataGovernance/subscribeManage'
 import addOrUpdate from './subscribeManage-add-or-update'
 import dispatchConfigAddOrUpdate from './dispatchConfig-add-or-update'
 import AssignPermission from '../../components/permission/assign-permission'
 export default {
-  data () {
+  data() {
     return {
+      historySqlList: [],
+      dialogTableVisible: false,
       dataList: [],
       dataListSelections: [],
       currPage: 1,
@@ -94,6 +260,7 @@ export default {
       addOrUpdateVisible: false,
       dispatchConfigAddOrUpdateVisible: false,
       dataForm: {
+        id: '',
         status: '',
         approveReason: '',
         type: ''
@@ -107,17 +274,31 @@ export default {
     }
   },
   components: { addOrUpdate, dispatchConfigAddOrUpdate, AssignPermission },
-  activated () {
+  activated() {
     this.getDataList()
   },
-  mounted () {
+  mounted() {
   },
   methods: {
+    historySql(item) {
+      watchHistorySql(item.id).then(({ data }) => {
+        if (data && data.code === 0) {
+          this.historySqlList = data.data
+          this.dialogTableVisible = true
+        } else {
+          this.$message({
+            message: data.message || '数据异常',
+            type: 'error'
+          })
+        }
+      })
+    },
     // 获取数据列表
-    getDataList () {
+    getDataList() {
       this.dataListLoading = true
       let params = {
         'approveReason': this.dataForm.approveReason,
+        'id': this.dataForm.id,
         'type': this.dataForm.type,
         'status': this.dataForm.status ? Number(this.dataForm.status) : '',
         'currPage': this.currPage,
@@ -136,21 +317,21 @@ export default {
       })
     },
     // 每页数
-    sizeChangeHandle (val) {
+    sizeChangeHandle(val) {
       this.pageSize = val
       this.currPage = 1
       this.getDataList()
     },
     // 当前页
-    currentChangeHandle (val) {
+    currentChangeHandle(val) {
       this.currPage = val
       this.getDataList()
     },
-    searchData () {
+    searchData() {
       this.currPage = 1
       this.getDataList()
     },
-    authHandle (row) {
+    authHandle(row) {
       return this.isAdmin || row.authOtherList.includes(this.userid || this.username) || row.authOwner === this.userid || row.authOwner === this.username
     },
     // searchImgData () {
@@ -158,21 +339,21 @@ export default {
     //   window.open(routeUrl.href, '_blank')
     // },
     // 新增 / 修改
-    addOrUpdateHandle (id) {
-      this.$router.push({ name: 'dataGovernance-dataServiceSql', query: {id: id} })
+    addOrUpdateHandle(id) {
+      this.$router.push({ name: 'dataGovernance-dataServiceSql', query: { id: id } })
     },
     // 调度配置
-    dispatchConfig (row) {
+    dispatchConfig(row) {
       this.dispatchConfigAddOrUpdateVisible = true
       this.$nextTick(() => {
         this.$refs.dispatchConfigAddOrUpdate.init(row)
       })
     },
     // 多选
-    selectionChangeHandle (val) {
+    selectionChangeHandle(val) {
       this.dataListSelections = val
     },
-    taskPermission (row) {
+    taskPermission(row) {
       // 打开权限分配弹框
       // 根据登陆用户和数据创建人判断是否是同一用户决定权限按钮是否显示
       this.assignPermissionVisible = true
@@ -181,7 +362,7 @@ export default {
       })
     },
     // 批量授权
-    multiTaskPermission () {
+    multiTaskPermission() {
       if (this.dataListSelections.length) {
         this.assignPermissionVisible = true
         let ids = this.dataListSelections.map(item => {
@@ -193,7 +374,7 @@ export default {
       }
     },
     // 上下线
-    upOrDownHandle (row) {
+    upOrDownHandle(row) {
       subscriptionUpAndDown(row.id).then(({ data }) => {
         if (data && data.code === 0) {
           this.$message({
