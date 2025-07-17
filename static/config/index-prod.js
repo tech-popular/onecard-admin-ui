e521d236afb6855f52fefc46ec1a6d4944a24c19
dingning <dingning@9fbank.com.cn>
2025-07-17 17:06:29 +0800

    画像增加渠道中易付

--- static/config/index-prod.js
+++ static/config/index-prod.js
@@ -12,10 +12,10 @@
     window.SITE_CONFIG['baseUrl'] = '//inner-tech.9f.cn/canary-admin'
   } else {
     // api接口请求地址
-    window.SITE_CONFIG['baseUrl'] = '//tech.9f.cn/canary-admin' // 测试环境
+    window.SITE_CONFIG['baseUrl'] = '/canary-admin'
   }
   // cdn地址 = 域名 + 版本号
-  window.SITE_CONFIG['BIGDATA_HREFLINK'] = 'http://tech.9f.cn' // 域名
+  window.SITE_CONFIG['BIGDATA_HREFLINK'] = 'http://unify-manager.datacenter-standard' // 域名
 
   window.SITE_CONFIG['domain'] = './' // 域名
   window.SITE_CONFIG['version'] = ''   // 版本号(年月日时分)
