823038bc25343d31f90596c30399cf39f92d14be
dingning <dingning@9fbank.com.cn>
2025-07-17 17:08:44 +0800

    rollback

--- static/config/index-prod.js
+++ static/config/index-prod.js
@@ -15,7 +15,7 @@
     window.SITE_CONFIG['baseUrl'] = '/canary-admin'
   }
   // cdn地址 = 域名 + 版本号
-  window.SITE_CONFIG['BIGDATA_HREFLINK'] = 'http://unify-manager.datacenter-standard' // 域名
+  window.SITE_CONFIG['BIGDATA_HREFLINK'] = 'http://tech.9f.cn' // 域名
 
   window.SITE_CONFIG['domain'] = './' // 域名
   window.SITE_CONFIG['version'] = ''   // 版本号(年月日时分)
